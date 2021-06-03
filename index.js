c1c7acbe5831267a2d1c7db491a87b7b92ccbde2
Elizabeth <66342338+ej040200@users.noreply.github.com>
2021-06-03 22:11:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ window.addEventListener('scroll',()=>{
 });
 // The createPost function creates The HTML for the blog post.
 // It append it to the container.
-function createPost(){
-	const post = document.createElement('div');
-	post.className = 'text';
-	post.innerHTML = `<h1>Lorem ipsum dolor sit amet</h1>
-	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque eos, atque sed saepe
-	   tempore, sequi qui excepturi voluptate ut perspiciatis culpa sit harum, corrupti ullam 
-	   voluptatibus obcaecati sint dignissimos quas.</p>`;
+//function createPost(){
+	//const post = document.createElement('div');
+	//post.className = 'text';
+	//post.innerHTML = `<h1>Lorem ipsum dolor sit amet</h1>
+	//<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque eos, atque sed saepe
+	  // tempore, sequi qui excepturi voluptate ut perspiciatis culpa sit harum, corrupti ullam 
+	   //voluptatibus obcaecati sint dignissimos quas.</p>`;
 //   Appending the post to the container.
-	container.appendChild(post);
+	//container.appendChild(post);
 }
